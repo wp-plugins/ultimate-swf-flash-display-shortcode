80a7fc031e753739203b76cb42f061703db9951c
kris.lopac <kris.lopac@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-21 07:23:33 +0000

    ultimate-swf-flash-display-shortcode.php
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-swf-flash-display-shortcode/trunk@1202897 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ultimate-swf-flash-display-shortcode.php
+++ ultimate-swf-flash-display-shortcode.php
@@ -59,7 +59,7 @@ add_shortcode('cd_flash', 'ultimate_swf_flash_display_shortcode');
 		<!--<![endif]-->
 		</object>
 ";
-$data_display .= "<div class='copy' style='color:#ccc; font-size: 9px; '><a href='http://crescentappraisal.com/' title='click here' target='_blank'>Estate Appraisal Seattle</a></div>";
+$data_display .= "<div class='copy' style='color:#ccc; font-size: 9px; '><a href='http://www.visualscopeasia.com/' title='click here' target='_blank'>VisualscopeAsia.com</a></div>";
 }
  	return $data_display;
  }
